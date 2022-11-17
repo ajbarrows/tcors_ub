461ab554ca2b75c51cf8f8e5dfe3602a363a734e
Anthony Barrows <ajbarrow@uvm.edu>
2022-11-17 11:44:58 -0500

    Make things Pythonic...

--- src/analysis/tcors_analysis.py
+++ src/analysis/tcors_analysis.py
@@ -1,19 +1,21 @@
 from tcors_functions import *
 
-random_state = 42
 
-# Load longitudinal data
-#s2_full = load_data("s2_data.csv")
+if __name__ == "__main__":
+    random_state = 42
 
-# Load Week 12 data
-w12_df = load_data("w12_recode.csv")
+    # Load longitudinal data
+    #s2_full = load_data("s2_data.csv")
+
+    # Load Week 12 data
+    w12_df = load_data("w12_recode.csv")
 
 
-# (Example)
+    # (Example)
 
-df_sub = w12_df[["screen_sex", "screen_age", "project", "baseline_cpd", "dose", "total_cpd"]]
+    df_sub = w12_df[["screen_sex", "screen_age", "project", "baseline_cpd", "dose", "total_cpd"]]
 
-X_train, X_test, y_train, y_test = make_training_split(df_sub, y = ["total_cpd"])
+    X_train, X_test, y_train, y_test = make_training_split(df_sub, y = ["total_cpd"])
 
-print(X_train)
-print(y_train)
+    print(X_train)
+    print(y_train)
