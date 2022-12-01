fce637064bac7957c5eb80de1a456d2d682bad61
Anthony Barrows <ajbarrow@uvm.edu>
2022-12-01 14:19:41 -0500

    Add plotting methods

--- src/visualize/visualize.py
+++ src/visualize/visualize.py
@@ -23,3 +23,34 @@ def make_pointplot(df, yvar, ylabel, color_var, legend_title, title, filename, s
         plt.savefig(fname, dpi = 200)
     
     plt.show()
+
+
+def plot_bins(df):
+
+    n_cat = df['prp_change_bin'].nunique()
+    df_plt = df.groupby(['dose', 'prp_change_bin'])['prp_change_bin'].size().reset_index(name = "counts")
+
+    sns.catplot(
+        data=df_plt,
+        x='prp_change_bin',
+        y="counts",
+        hue = 'dose',
+        kind="bar"
+    )
+    plt.xticks(rotation = 45)
+    plt.xlabel("Binned Proportion of Baseline CPD")
+    plt.ylabel("Subjects")
+    plt.suptitle("Categorical Model of Prp. Change in CPD from Baseline", y = 1)
+    plt.text(x=1, y=40, s= str(n_cat) + " bins")
+
+def clf_plt(df):
+    p = sns.catplot(
+        data = df,
+        x = 'model',
+        y = 'f1',
+        hue = 'label',
+        kind = 'bar'
+    )
+    p.legend.set_title(None)
+    plt.title("Classification Performance")
+
